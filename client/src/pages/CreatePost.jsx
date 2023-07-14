97c2d8e5e2c683c3c00fc6cbc8e372ff61a41668
maikolcw <whysoraccoony@gmail.com>
2023-07-13 21:49:09 -0700

    added area for generated image in createpost

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,6 +17,10 @@ const CreatePost = () => {
     const [generatingImg, setGeneratingImg] = useState(false);
     const [loading, setLoading] = useState(false);
 
+    const generateImage = async () => {
+        
+    };
+
     const handleSubmit = async () => {
         e.preventDefault();
 
@@ -45,7 +49,7 @@ const CreatePost = () => {
                         labelName="Your Name"
                         type="text"
                         name="name"
-                        placeholder="John Doe"
+                        placeholder="Jane Doe"
                         value={form.name}
                         handleChange={handleChange}
                     />
@@ -54,12 +58,54 @@ const CreatePost = () => {
                         labelName="Prompt"
                         type="text"
                         name="prompt"
-                        placeholder="A stunning glowing figure floating above a black hole in outer space"
+                        placeholder="An stunningly beautiful angelic figure floating above a black hole in outer space..."
                         value={form.prompt}
                         handleChange={handleChange}
                         isSurpriseMe
                         handleSurpriseMe={handleSurpriseMe}
                     />
+
+                    <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+                        {form.photo ? (
+                            <img
+                                src={form.photo}
+                                alt={form.prompt}
+                                className="w-full h-full object-contain"
+                            />
+                        ) : (
+                            <img
+                                src={preview}
+                                alt="preview"
+                                className="w-9/12 h-9/12 object-contain opacity-40"
+                            />
+                        )}
+
+                        {generatingImg && (
+                            <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                                <Loader />
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                <div className="mt-5 flex gap-5">
+                    <button
+                        type="button"
+                        onClick={generateImage}
+                        className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                    >
+                        {generatingImg ? 'Generating...' : 'Generate'}
+                    </button>
+                </div>
+
+                <div className="mt-10">
+                    <p className="mt-2 text-[#666e75] text-[14px]">** You can share this image with the community! **</p>
+                    <button
+                        type="submit"
+                        className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                    >
+                        {loading ? 'Sharing...' : 'Share with the Community'}
+                    </button>
                 </div>
             </form>
         </section>
