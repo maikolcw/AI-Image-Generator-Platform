f42973588846373845b642c877949388cc9f2737
maikolcw <whysoraccoony@gmail.com>
2023-07-14 18:11:54 -0700

    change mongo user pw, added dalle get route to test configuration

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -6,6 +6,17 @@ dotenv.config();
 
 const router = express.Router();
 
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+router.route('/').get((req, res) => {
+    res.status(200).json({ message: 'Hello from dall-e waurdo' });
+});
+
+
 
 
 export default router;
