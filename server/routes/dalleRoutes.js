ac9ffb2d845359e27b7250bd2668365cd6dee0f3
maikolcw <whysoraccoony@gmail.com>
2024-08-02 22:22:11 -0700

    revert

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -20,14 +20,14 @@ router.route('/').post(async (req, res) => {
     try {
         const { prompt } = req.body;
 
-        const aiResponse = await openai.images.generate({
+        const aiResponse = await openai.createImage({
             prompt,
             n: 1,
             size: '1024x1024',
             response_format: 'b64_json',
         });
-        console.log(aiResponse)
-        const image = aiResponse.data[0].b64_json;
+
+        const image = aiResponse.data.data[0].b64_json;
         res.status(200).json({ photo: image });
     } catch (error) {
         console.error(error);
