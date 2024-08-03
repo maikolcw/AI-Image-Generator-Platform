e3b83fb4481a27a6f4d0d639674253df3290ca9c
maikolcw <whysoraccoony@gmail.com>
2024-08-02 21:38:31 -0700

    change createImage to images.generate

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -20,7 +20,7 @@ router.route('/').post(async (req, res) => {
     try {
         const { prompt } = req.body;
 
-        const aiResponse = await openai.createImage({
+        const aiResponse = await openai.images.generate({
             prompt,
             n: 1,
             size: '1024x1024',
